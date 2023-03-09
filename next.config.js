ab08baf747ffc56120837ac445ebba2f1299bbf4
brunolepis <brunka215@gmail.com>
2023-03-09 17:04:17 +0100

    fix: remove webpack config

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        "original-fs": false,
-        "fs": false,
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
